5ec8af684bb212ea6e9d1fe81ad346cd4b906427
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-02-13 14:38:36 +0100

    feat: generate scaffold for Person

---
+++ db/migrate/20250213133808_create_people.rb
@@ -0,0 +1,13 @@
+class CreatePeople < ActiveRecord::Migration[8.0]
+  def change
+    create_table :people do |t|
+      t.string :lastname, null: false
+      t.string :firstname, null: false
+      t.string :email, null: false
+      t.string :phone_number
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
