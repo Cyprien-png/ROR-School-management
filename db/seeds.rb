4baf2fcd51cf311a81d4806918005359d67c17ed
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-02-14 17:47:30 +0100

    test: add a temporary seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create a Teacher
+Teacher.create(
+  lastname: "Doe",
+  firstname: "John",
+  email: "john.doe@example.com",
+  phone_number: "1234567890",
+  iban: "GB29NWBK60161331926819",
+  password: "password",
+  password_confirmation: "password"
+)
+
+# Create a Student
+Student.create(
+  lastname: "Smith",
+  firstname: "Jane",
+  email: "jane.smith@example.com",
+  phone_number: "0987654321",
+  status: 0,
+  password: "password",
+  password_confirmation: "password"
+)
+
+# Create a Dean
+Dean.create(
+  lastname: "Johnson",
+  firstname: "Emily",
+  email: "emily.johnson@example.com",
+  phone_number: "5551234567",
+  password: "password",
+  password_confirmation: "password"
+)
