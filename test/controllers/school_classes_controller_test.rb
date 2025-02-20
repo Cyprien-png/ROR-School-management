d33196e2cfc27ca0d1db3159a8ccaf655c20be8b
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-02-20 12:12:35 +0100

    test: setup base tests on the shcool_classes

--- test/controllers/school_classes_controller_test.rb
+++ test/controllers/school_classes_controller_test.rb
@@ -2,7 +2,47 @@ require "test_helper"
 
 class SchoolClassesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @school_class = school_classes(:one)
+    # Create and sign in a dean for authentication
+    @dean = Dean.create!(
+      lastname: "Admin",
+      firstname: "Test",
+      email: "admin@test.com",
+      phone_number: "1234567890",
+      password: "password",
+      password_confirmation: "password"
+    )
+    
+    # Sign in the dean
+    post person_session_path, params: { 
+      person: { 
+        email: @dean.email, 
+        password: "password" 
+      } 
+    }
+
+    # Create a teacher for the school class
+    @teacher = Teacher.create!(
+      lastname: "Smith",
+      firstname: "John",
+      email: "john.smith@test.com",
+      phone_number: "9876543210",
+      iban: "GB29NWBK60161331926819",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    # Create a test school class
+    @school_class = SchoolClass.create!(
+      name: "Test Class",
+      grade: 1,
+      year: 2025,
+      teacher: @teacher
+    )
+  end
+
+  teardown do
+    SchoolClass.delete_all
+    Person.delete_all
   end
 
   test "should get index" do
@@ -17,7 +57,14 @@ class SchoolClassesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create school_class" do
     assert_difference("SchoolClass.count") do
-      post school_classes_url, params: { school_class: { grade: @school_class.grade, name: @school_class.name, year: @school_class.year } }
+      post school_classes_url, params: { 
+        school_class: { 
+          name: "New Class",
+          grade: 2,
+          year: 2025,
+          teacher_id: @teacher.id
+        } 
+      }
     end
 
     assert_redirected_to school_class_url(SchoolClass.last)
@@ -34,7 +81,14 @@ class SchoolClassesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update school_class" do
-    patch school_class_url(@school_class), params: { school_class: { grade: @school_class.grade, name: @school_class.name, year: @school_class.year } }
+    patch school_class_url(@school_class), params: { 
+      school_class: { 
+        name: "Updated Class",
+        grade: 3,
+        year: 2026,
+        teacher_id: @teacher.id
+      } 
+    }
     assert_redirected_to school_class_url(@school_class)
   end
 
