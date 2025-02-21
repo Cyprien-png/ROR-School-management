3006aa1ba95c875eb9219a52cd594bb4c1a08f8b
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-02-21 15:29:17 +0100

    test: add test for the access rights on the school_classes actions/controller endpoints

--- test/controllers/school_classes_controller_test.rb
+++ test/controllers/school_classes_controller_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class SchoolClassesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     # Create and sign in a dean for authentication
     @dean = Dean.create!(
@@ -11,14 +13,6 @@ class SchoolClassesControllerTest < ActionDispatch::IntegrationTest
       password: "password",
       password_confirmation: "password"
     )
-    
-    # Sign in the dean
-    post person_session_path, params: { 
-      person: { 
-        email: @dean.email, 
-        password: "password" 
-      } 
-    }
 
     # Create a teacher for the school class
     @teacher = Teacher.create!(
@@ -27,6 +21,17 @@ class SchoolClassesControllerTest < ActionDispatch::IntegrationTest
       email: "john.smith@test.com",
       phone_number: "9876543210",
       iban: "GB29NWBK60161331926819",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    # Create a student for testing unauthorized access
+    @student = Student.create!(
+      lastname: "Doe",
+      firstname: "Jane",
+      email: "jane.doe@test.com",
+      phone_number: "5555555555",
+      status: :in_formation,
       password: "password",
       password_confirmation: "password"
     )
@@ -45,58 +50,158 @@ class SchoolClassesControllerTest < ActionDispatch::IntegrationTest
     Person.delete_all
   end
 
-  test "should get index" do
+  test "should get index when signed in as any user type" do
+    # Test as dean
+    sign_in_as(@dean)
+    get school_classes_url
+    assert_response :success
+
+    # Test as teacher
+    sign_in_as(@teacher)
+    get school_classes_url
+    assert_response :success
+
+    # Test as student
+    sign_in_as(@student)
     get school_classes_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "should get new when dean" do
+    sign_in_as(@dean)
     get new_school_class_url
     assert_response :success
   end
 
-  test "should create school_class" do
+  test "should not get new when teacher" do
+    sign_in_as(@teacher)
+    get new_school_class_url
+    assert_redirected_to root_path
+    assert_equal "Only deans can manage school classes.", flash[:alert]
+  end
+
+  test "should not get new when student" do
+    sign_in_as(@student)
+    get new_school_class_url
+    assert_redirected_to root_path
+    assert_equal "Only deans can manage school classes.", flash[:alert]
+  end
+
+  test "should create school_class when dean" do
+    sign_in_as(@dean)
     assert_difference("SchoolClass.count") do
-      post school_classes_url, params: { 
-        school_class: { 
+      post school_classes_url, params: {
+        school_class: {
           name: "New Class",
           grade: 2,
           year: 2025,
           teacher_id: @teacher.id
-        } 
+        }
       }
     end
 
     assert_redirected_to school_class_url(SchoolClass.last)
   end
 
-  test "should show school_class" do
+  test "should not create school_class when teacher" do
+    sign_in_as(@teacher)
+    assert_no_difference("SchoolClass.count") do
+      post school_classes_url, params: {
+        school_class: {
+          name: "New Class",
+          grade: 2,
+          year: 2025,
+          teacher_id: @teacher.id
+        }
+      }
+    end
+    assert_redirected_to root_path
+    assert_equal "Only deans can manage school classes.", flash[:alert]
+  end
+
+  test "should show school_class for any user type" do
+    # Test as dean
+    sign_in_as(@dean)
+    get school_class_url(@school_class)
+    assert_response :success
+
+    # Test as teacher
+    sign_in_as(@teacher)
+    get school_class_url(@school_class)
+    assert_response :success
+
+    # Test as student
+    sign_in_as(@student)
     get school_class_url(@school_class)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when dean" do
+    sign_in_as(@dean)
     get edit_school_class_url(@school_class)
     assert_response :success
   end
 
-  test "should update school_class" do
-    patch school_class_url(@school_class), params: { 
-      school_class: { 
+  test "should not get edit when teacher" do
+    sign_in_as(@teacher)
+    get edit_school_class_url(@school_class)
+    assert_redirected_to root_path
+    assert_equal "Only deans can manage school classes.", flash[:alert]
+  end
+
+  test "should update school_class when dean" do
+    sign_in_as(@dean)
+    patch school_class_url(@school_class), params: {
+      school_class: {
         name: "Updated Class",
         grade: 3,
         year: 2026,
         teacher_id: @teacher.id
-      } 
+      }
     }
     assert_redirected_to school_class_url(@school_class)
   end
 
-  test "should destroy school_class" do
+  test "should not update school_class when teacher" do
+    sign_in_as(@teacher)
+    patch school_class_url(@school_class), params: {
+      school_class: {
+        name: "Updated Class",
+        grade: 3,
+        year: 2026,
+        teacher_id: @teacher.id
+      }
+    }
+    assert_redirected_to root_path
+    assert_equal "Only deans can manage school classes.", flash[:alert]
+  end
+
+  test "should destroy school_class when dean" do
+    sign_in_as(@dean)
     assert_difference("SchoolClass.count", -1) do
       delete school_class_url(@school_class)
     end
 
     assert_redirected_to school_classes_url
+  end
+
+  test "should not destroy school_class when teacher" do
+    sign_in_as(@teacher)
+    assert_no_difference("SchoolClass.count") do
+      delete school_class_url(@school_class)
+    end
+    assert_redirected_to root_path
+    assert_equal "Only deans can manage school classes.", flash[:alert]
+  end
+
+
+  private def sign_in_as(person)
+    sign_out :person
+    post person_session_path, params: {
+      person: {
+        email: person.email,
+        password: "password"
+      }
+    }
   end
 end
