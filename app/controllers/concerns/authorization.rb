248fe9491011b5571f629be1629ce5826971ff62
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-02-21 15:18:57 +0100

    feat: make the authorization checker for school_classes actions returns boolean

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -2,9 +2,11 @@ module Authorization
   extend ActiveSupport::Concern
 
   private def authorize_dean
-    unless current_person.is_a?(Dean)
+    unless current_person&.is_a?(Dean)
       flash[:alert] = "Only deans can manage school classes."
       redirect_to root_path
+      return false
     end
+    return true
   end
 end 
