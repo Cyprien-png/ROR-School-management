7953939b40ee656239b8d4ca52b05fc5723a0253
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-09 15:07:36 +0100

    fix: remove the %> visible in the UI

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,16 +4,10 @@ module.exports = {
       './public/*.html',
       './app/helpers/**/*.rb',
       './app/javascript/**/*.js',
-      './app/views/**/*.{erb,haml,html,slim}',
-      './app/components/**/*.{erb,haml,html,slim,rb}',
-      './app/assets/stylesheets/**/*.css',
+      './app/views/**/*',
     ],
     theme: {
       extend: {},
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/aspect-ratio'),
-      require('@tailwindcss/typography'),
-    ],
+    plugins: [],
   }
